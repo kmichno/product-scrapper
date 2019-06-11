140f83c753ba7eff88c1815a909dd4050c07ebcf
Łukasz <lukasz.lepak@protonmail.com>
2019-06-11 23:27:50 +0200

    add allegro-spider to start script

--- go-spider.py
+++ go-spider.py
@@ -1,9 +1,11 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
+import sys
+
 from product_scrapper.spiders.product_ceneo_spider import ProductCeneoSpider
 
-
 process = CrawlerProcess(get_project_settings())
-process.crawl('product_ceneo', phrase='Brother', task_id='1')
+process.crawl('product_ceneo', phrase=sys.argv[1], task_id=sys.argv[2])
+process.crawl('product_allegro', phrase=sys.argv[1], task_id=sys.argv[2])
 process.start()
